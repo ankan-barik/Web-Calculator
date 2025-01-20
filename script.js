65551c5d616cbfdde56c1a50a14b7a9b89f97acc
ankan-barik <144473862+ankan-barik@users.noreply.github.com>
2025-01-20 23:46:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Todo: Make M+ M- and MC functional
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
+let display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid input');
+        clearDisplay();
     }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+}
